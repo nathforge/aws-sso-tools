8f011f6953fd94597b57f99f4c689aa2d9bc9a8e
Nathan Reynolds <email@nreynolds.co.uk>
2026-07-22 13:29:25 +0100

    Use builder: rust; pre-build extras in scripts/build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-BINARIES=(
-    docker-credential-sso-ecr-login
+EXTRA_BINARIES=(
     aws-sso-should-login
     aws-sso-maybe-login
     aws-sso-login-showing-code
@@ -14,9 +13,9 @@ rustup target add aarch64-apple-darwin x86_64-apple-darwin
 (cd rust && cargo build --release --target aarch64-apple-darwin)
 (cd rust && cargo build --release --target x86_64-apple-darwin)
 
-mkdir -p dist/prebuilt/darwin_arm64 dist/prebuilt/darwin_amd64
+mkdir -p dist/extras/darwin_arm64 dist/extras/darwin_amd64
 
-for b in "${BINARIES[@]}"; do
-    cp "rust/target/aarch64-apple-darwin/release/$b" "dist/prebuilt/darwin_arm64/$b"
-    cp "rust/target/x86_64-apple-darwin/release/$b" "dist/prebuilt/darwin_amd64/$b"
+for b in "${EXTRA_BINARIES[@]}"; do
+    cp "rust/target/aarch64-apple-darwin/release/$b" "dist/extras/darwin_arm64/$b"
+    cp "rust/target/x86_64-apple-darwin/release/$b" "dist/extras/darwin_amd64/$b"
 done
