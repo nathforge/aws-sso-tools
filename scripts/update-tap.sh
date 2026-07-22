9d65afdf5b841bc938f8051334b9c61545d08f44
Nathan Reynolds <email@nreynolds.co.uk>
2026-07-22 16:27:13 +0100

    Add Linux support to Homebrew tap

--- scripts/update-tap.sh
+++ scripts/update-tap.sh
@@ -4,8 +4,10 @@ set -euo pipefail
 VERSION="$1"
 VERSION_NUM="${VERSION#v}"
 
-ARM64_SHA=$(shasum -a 256 dist/aws-sso-tools_darwin_arm64.tar.gz | awk '{print $1}')
-AMD64_SHA=$(shasum -a 256 dist/aws-sso-tools_darwin_amd64.tar.gz | awk '{print $1}')
+DARWIN_ARM64_SHA=$(shasum -a 256 dist/aws-sso-tools_darwin_arm64.tar.gz | awk '{print $1}')
+DARWIN_AMD64_SHA=$(shasum -a 256 dist/aws-sso-tools_darwin_amd64.tar.gz | awk '{print $1}')
+LINUX_ARM64_SHA=$(shasum -a 256 dist/aws-sso-tools_linux_arm64.tar.gz | awk '{print $1}')
+LINUX_AMD64_SHA=$(shasum -a 256 dist/aws-sso-tools_linux_amd64.tar.gz | awk '{print $1}')
 
 cat > /tmp/aws-sso-tools.rb <<RUBY
 # typed: false
@@ -20,11 +22,22 @@ class AwsSsoTools < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/nathforge/aws-sso-tools/releases/download/${VERSION}/aws-sso-tools_darwin_arm64.tar.gz"
-      sha256 "${ARM64_SHA}"
+      sha256 "${DARWIN_ARM64_SHA}"
     end
     if Hardware::CPU.intel?
       url "https://github.com/nathforge/aws-sso-tools/releases/download/${VERSION}/aws-sso-tools_darwin_amd64.tar.gz"
-      sha256 "${AMD64_SHA}"
+      sha256 "${DARWIN_AMD64_SHA}"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/nathforge/aws-sso-tools/releases/download/${VERSION}/aws-sso-tools_linux_arm64.tar.gz"
+      sha256 "${LINUX_ARM64_SHA}"
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/nathforge/aws-sso-tools/releases/download/${VERSION}/aws-sso-tools_linux_amd64.tar.gz"
+      sha256 "${LINUX_AMD64_SHA}"
     end
   end
 
